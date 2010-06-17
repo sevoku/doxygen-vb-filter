77d624abd989fa7d07cdbccd9a2c5b206fdd9348
Vsevolod Kukol <sevo@sevo.org>
2010-06-17 00:19:27 +0000

    - add operator - add classic vb type

---
+++ samples/vb_type.vb
@@ -0,0 +1,13 @@
+' <summary>example file demonstrating classic vb types</summary>
+' <remarks>
+' The original unfiltered source of this file : <A HREF="http://trac.sevo.org/projects/doxyvb/browser/trunk/samples/vb_type.vb">vb_type.vb</A></remarks>
+
+''' <summary>
+''' simple VB6 Type
+''' </summary>
+Public Type SampleVB6Type
+
+    Public someInteger As Integer ' simple private integer value
+    Public someString As String   ' simple public string value
+
+End Type
